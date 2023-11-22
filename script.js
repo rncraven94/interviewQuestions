3bbabfe62408d02414626dc3db2377a6bcfc4c1c
rncraven94 <neilcraven2013@gmail.com>
2023-11-21 21:13:13 -0500

    script.js

--- script.js
+++ script.js
@@ -84,3 +84,45 @@ console.log(strings);
 //splice
 strings.splice(2, 0, "alien");
 console.log(strings);
+
+////////////////////////////////////////////// implementing an array
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index];
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+  }
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const newArray = new MyArray();
+newArray.push("hi");
+newArray.push("fuck");
+newArray.push("you");
+// newArray.pop();
+newArray.push("!");
+newArray.delete(1);
+console.log(newArray);
